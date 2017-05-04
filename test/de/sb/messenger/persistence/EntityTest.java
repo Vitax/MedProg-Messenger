6557bdcc4aacb79826395e144015d52fadfa65b8
dynex11 <ralen4e@yahoo.com>
2017-05-04 22:11:26 +0200

    Empty basket method
    clear up the backet and delete the objects

--- test/de/sb/messenger/persistence/EntityTest.java
+++ test/de/sb/messenger/persistence/EntityTest.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.fail;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,21 +30,40 @@ public class EntityTest {
 		EM_FACTORY = Persistence.createEntityManagerFactory("messenger");
 		VALIDATOR_FACTORY = Validation.buildDefaultValidatorFactory();
 	}
+
 	// creates a persistent unit "messenger"
 	public EntityManagerFactory getEntityManagerFactory() {
 
 		return EM_FACTORY;
 
 	}
-	
-	public ValidatorFactory getEntityValidatorFactory(){
+
+	public ValidatorFactory getEntityValidatorFactory() {
 		return VALIDATOR_FACTORY;
-		
+
 	}
-	
-	public Set<Long> getWasteBasket(){
+
+	public Set<Long> getWasteBasket() {
 		return wasteBasket;
 	}
+	
+	@After
+	public void emptyWasteBasket () {
+		final EntityManager entityManager = EM_FACTORY.createEntityManager();
+		try{
+			entityManager.getTransaction().begin();
+			for (final Long identity : this.wasteBasket) {
+				
+					final Object entity = entityManager.find(BaseEntity.class, identity);
+					if (entity != null) entityManager.remove(entity);
+			}
+			entityManager.getTransaction().commit();
+			this.wasteBasket.clear();
+		} finally {
+			entityManager.close();
+		}
+	}
+
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
