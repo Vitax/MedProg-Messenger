d039bab196d8ce69a755120a89301a14aa0ae710
Mario Link <gilgatx@gmail.com>
2017-05-05 12:06:23 +0200

    Update Person.java

--- src/de/sb/messenger/persistence/Person.java
+++ src/de/sb/messenger/persistence/Person.java
@@ -57,7 +57,7 @@ public class Person extends BaseEntity {
 	@JoinColumn(name="identity")
 	private Document avatar;
 	
-	@OneToMany
+	@OneToMany(mappedBy = "identity")
 	private Set<Message> messages;
 	
 	@ManyToMany(mappedBy = "peopleObserved_REL") //cascade = CascadeType.REMOVE
