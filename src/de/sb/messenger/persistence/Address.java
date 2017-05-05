5628846b9bd4c6bfe202c9de24126866f8e89ea1
Mario Link <gilgatx@gmail.com>
2017-05-05 12:17:47 +0200

    Update Address.java

--- src/de/sb/messenger/persistence/Address.java
+++ src/de/sb/messenger/persistence/Address.java
@@ -10,8 +10,10 @@ public class Address {
 
 	@Size(min = 0, max = 63) @Pattern(regexp = "^([^0-9]{2,} [0-9]+)?$")
 	private String street;
+	
 	@Size(min = 0, max = 15) @Pattern(regexp = "^[0-9]*$")
 	private String postcode;
+	
 	@NotNull @Size(min = 1, max = 63) @Pattern(regexp = "^([^0-9]{2,})?$")
 	private String city;
 
