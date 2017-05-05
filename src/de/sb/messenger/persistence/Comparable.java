8e2b6b64e3a66e260b0920b07e49990c82bff807
dynex11 <ralen4e@yahoo.com>
2017-05-05 10:14:07 +0200

    Document inherit BaseEntity, remove comparable

--- src/de/sb/messenger/persistence/Comparable.java
+++
@@ -1,7 +0,0 @@
-package de.sb.messenger.persistence;
-
-public abstract interface Comparable {
-   // declare fields
-   // declare nonabstract methods
-   abstract int compareTo(final BaseEntity obj);
-}
