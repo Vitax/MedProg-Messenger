d9a6d67b378bbaa65427468e4a059d0d59edcb41
Sonja Frey <Sonja.Frey@student.htw-berlin.de>
2017-05-16 10:57:40 +0200

    add cases polym field subject

--- src/de/sb/messenger/persistence/MessageEntityTest.java
+++ src/de/sb/messenger/persistence/MessageEntityTest.java
@@ -1,6 +1,5 @@
 package de.sb.messenger.persistence;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -9,6 +8,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
@@ -66,37 +66,75 @@ public class MessageEntityTest extends EntityTest {
 	@Test
 	public void testLifeCycle() throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		// create entity
-		Person person = new Person("test@gmail.com", new Document("image/png", null));
+		String s = "some content";
+		byte[] content = s.getBytes();
+		Document doc = new Document("image/jpeg", content);
+		Person person = new Person("test@gmail.com", doc);
 		person.getName().setGiven("John");
 		person.getName().setFamily("Smith");
 		person.getAddress().setStreet("Falkenbergerstr. 1");
 		person.getAddress().setPostcode("12345");
 		person.getAddress().setCity("Berlin");
-		person.setGroup(Group.USER);
+		
 		byte[] hash = person.passwordHash("password");
 		person.setPasswordHash(hash);
+		
+		
+		//case 1
+		//Person poly_subject = new Person("subject@gmail.com", doc);
+		//poly_subject.getName().setGiven("Poly");
+		//poly_subject.getName().setFamily("Subject");
+		//poly_subject.getAddress().setStreet("Falkenbergerstr. 1");
+		//poly_subject.getAddress().setPostcode("12345");
+		//poly_subject.getAddress().setCity("Berlin");
+		//byte[] hash2 = poly_subject.passwordHash("password");
+		//poly_subject.setPasswordHash(hash2);
+		
+		//case 2
+		Document poly_subject = new Document("image/jpeg", content);
+			
 
-
-		BaseEntity baseEntity = new BaseEntity();
-		Message message = new Message(person, baseEntity, "Hi there!");
+		
+		//BaseEntity subject = new BaseEntity();
+		Message message = new Message(person, doc, "Hi there!");
+		
+		
+		//case 3
+		//Message poly_subject = new Message(person, message, "Hi there!");
+		
+		
+		
 		// // add to the DB
-		entityManager.getTransaction().begin();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(doc);
+		entityManager.getTransaction().commit();
+		transaction.begin();
+		entityManager.persist(person);
+		entityManager.getTransaction().commit();
+		//transaction.begin();
+		//entityManager.persist(poly_subject);
+		//entityManager.getTransaction().commit();
+		transaction.begin();
 		entityManager.persist(message);
 		entityManager.getTransaction().commit();
-		this.getWasteBasket().add(person.getIdentiy());
-		entityManager.getTransaction().begin();
-
-		message = entityManager.find(Message.class, message.getIdentiy());
-		assertEquals(message.getAuthor(), person);
-		assertEquals(message.getBody(), "Hi there!");
-		assertEquals(message.getIdentiy(), baseEntity);
 		
-		// remove person from DB
-		entityManager.remove(message);
-		entityManager.getTransaction().commit();
-		// check if it's deleted , find for getter , Reference for setter
-		entityManager.find(Message.class, message.getIdentiy());
-		assertNull(message);
+		this.getWasteBasket().add(doc.getIdentiy());
+		this.getWasteBasket().add(person.getIdentiy());
+		//this.getWasteBasket().add(poly_subject.getIdentiy());
+		this.getWasteBasket().add(message.getIdentiy());
+		
+//		transaction.begin();
+//		message = entityManager.find(Message.class, message.getIdentiy());
+//		assertEquals(message.getAuthor(), person);
+//		assertEquals(message.getBody(), "Hi there!");
+//		assertEquals(message.getIdentiy(), baseEntity);
+//		
+//		// remove message from DB
+//		entityManager.remove(message);
+//		transaction.commit();
+//		// check if it's deleted , find for getter , Reference for setter	
+//		assertNull(entityManager.find(Message.class, message.getIdentiy()));
 
 	}
 
@@ -107,3 +145,5 @@ public class MessageEntityTest extends EntityTest {
 
 	}
 }
+
+
